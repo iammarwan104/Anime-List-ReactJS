a1b3e6d21fa3fc508a3fe188983525d0b7753d14
Marwan <marwahpinrang6@gmail.com>
2024-03-23 13:36:27 +0800

    add card file

---
+++ src/components/card-movie/Card.jsx
@@ -0,0 +1,21 @@
+export default function CardMovie({urlImage, title}) {
+  return (
+    <>
+      <div className="card pt-4 w-full bg-base-100 shadow-xl hover:bg-slate-800 duration-100">
+        <figure>
+          <img
+            src={urlImage}
+            alt={title}
+            width={300}
+            height={400}
+            loading="lazy"
+            className="w-[90%] rounded-lg"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+        </div>
+      </div>
+    </>
+  );
+}
